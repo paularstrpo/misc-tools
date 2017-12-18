3fd0e2bf7de816f2f39d5f94dae366ccfb8ce296
Paula <paularstrpo@gmail.com>
2017-12-18 14:20:14 -0500

    updated scripts

--- install_softwares.sh
+++ install_softwares.sh
@@ -25,7 +25,7 @@ cd $bwa_version
 make
 
 BWA_PATH=${ScriptHome}/$bwa_version
-find -name $BWA_PATH -printf
+#find -name $BWA_PATH -type d -printf
 
 # git clone https://github.com/lh3/bwa.git
 # cd bwa; make
@@ -45,14 +45,77 @@ tar -vxjf ${gatk_version}.tar.bz2
 
 mv ${gatk_version}*/GenomeAnalysisTK.jar ${ScriptHome}/${gatk_version}.jar
 GATK=${ScriptHome}/${gatk_version}.jar
-find -name $GATK -printf
+#find -name $GATK -printf
 
 # ----------------------------------------------------------------------------
 # 3. Picard Tools - (http://broadinstitute.github.io/picard/)
 # ----------------------------------------------------------------------------
-
 cd ${ScriptHome}
 picard_version="2.16.0"
 curl -L https://github.com/broadinstitute/picard/releases/download/${picard_version}/picard.jar -O ${ScriptHome}/"picard-${picard_version}.jar"
+tar -vxjf ${picard_version}.tar.bz2
 PICARD=${ScriptHome}/"picard-${picard_version}.jar"
-find -name $PICARD -printf
+#find -name $PICARD -printf
+
+# ----------------------------------------------------------------------------
+# 4. Samtools, BCFtools & HTSlib - (http://www.htslib.org/)
+# ----------------------------------------------------------------------------
+cd ${ScriptHome}
+samtools_version="samtools-1.6"
+bcftools_version="bcftools-1.6"
+htslib_version="htslib-1.6"
+
+
+curl -L -O https://github.com/samtools/samtools/releases/download/1.6/${samtools_version}.tar.bz2
+curl -L -O https://github.com/samtools/bcftools/releases/download/1.6/${bcftools_version}.tar.bz2
+curl -L -O https://github.com/samtools/htslib/releases/download/1.6/${htslib_version}.tar.bz2
+
+tar -vxjf ${samtools_version}.tar.bz2
+tar -vxjf ${bcftools_version}.tar.bz2
+tar -vxjf ${htslib_version}.tar.bz2
+
+
+./configure --prefix=${samtools_version}
+make
+
+./configure --prefix=${bcftools_version}
+make
+
+./configure --prefix=${htslib_version}
+make
+
+
+# ----------------------------------------------------------------------------
+# 5. Homebrew (MacOS only)
+# ----------------------------------------------------------------------------
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# ----------------------------------------------------------------------------
+# 6. Circos (MacOS version)
+# ----------------------------------------------------------------------------
+#
+# #Install requisite perl libraries
+# brew install cpanminus
+# cpanm Config::General Font::TTF::Font Math::Bezier Math::VecStat Readonly Set::IntSpan Text::Format Statistics::Basic SVG
+#
+# mkdir ${ScriptHome}/lib ; cd ${ScriptHome}/lib
+# curl -O http://wangqinhu.com/data/gd/gd.tar.gz
+# tar -zxf gd.tar.gz
+# cd gd
+# sudo ./install
+#
+# #Install Circos
+# mkdir ${ScriptHome}/circos ; cd ${ScriptHome}/circos
+# circos_pkg=''
+# circos_tools=''
+# circos_tut=''
+#
+# curl -O http://circos.ca/distribution/${circos_pkg}.tgz
+# curl -O http://circos.ca/distribution/${circos_tools}.tgz
+# curl -O http://circos.ca/distribution/${circos_tut}.tgz
+#
+# tar xvfz $circos_pkg
+# tar xvfz $circos_tools
+# tar xvfz $circos_tut
+#
+# ln -s $circos_pkg current
